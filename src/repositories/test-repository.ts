b2cfd2b1993727b385503afa50eb1a0e203d2415
Renat <jeingo20@gmail.com>
2023-01-02 15:41:45 +0300

    add deleting comments db

--- src/repositories/test-repository.ts
+++ src/repositories/test-repository.ts
@@ -1,9 +1,10 @@
-import {blogsCollection, postsCollection, usersCollection} from "./db"
+import {blogsCollection, commentsCollection, postsCollection, usersCollection} from "./db"
 
 export const testRepository = {
     async deleteAllDB(): Promise<void> {
         await blogsCollection.deleteMany({})
         await postsCollection.deleteMany({})
         await usersCollection.deleteMany({})
+        await commentsCollection.deleteMany({})
     }
 }
